0b0728e173e1f2a357af7b621a7512a4018401ed
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-16 21:55:13 -0800

    Work on youtube api

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,33 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    // key: options.key,
+    // q: options.query,
+    // maxResults: options.max,
+    // dataType: 'jsonp',
+
+    videoEmbeddable: true,
+    success: function() {
+      console.log('IT WORKED');
+      callback();
+    },
+    error: function() {
+      console.log(':(');
+      console.log(options);
+    }
+  });
+
+
+
+
 };
+
+
+
+
+
+
 
 window.searchYouTube = searchYouTube;
