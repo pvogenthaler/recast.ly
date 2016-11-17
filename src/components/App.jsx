8db8a0b85f113d6816f2804910622d062b611974
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-17 12:32:58 -0800

    Work on search input update videos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,8 @@ class App extends React.Component {
     super(props);
     this.state = {      // 2 states, search/ video list and video player
       currentVideo: window.exampleVideoData[0],
-      videos: window.exampleVideoData
+      videos: window.exampleVideoData,
+      value: 'cat'
     };
   }
   click(video) {
@@ -26,12 +27,21 @@ class App extends React.Component {
       currentVideo: video
     });
   }
+  searchClick(event) {
+    this.setState({
+      value: event.target.value
+    });
+    console.log(window.searchYouTube);
+    // window.searchYouTube({query: this.state.input, key: window.YOUTUBE_API_KEY, maxResults: 5}, (data) => {
+    //   this.setState({videos: data});
+    // });
+  }
   render() {
 
 // pass click to video list entry
     return (
       <div>
-        <Nav />
+        <Nav searchClick={this.searchClick.bind(this)} />
         <div className="col-md-7">
           <VideoPlayer 
             // video = {window.exampleVideoData[0]} 
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,34 +1,24 @@
 var searchYouTube = (options, callback) => {
-  // TODO
   $.ajax({
-    url: 'https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&key=' + options.key + '&q=' 
-    + options.query + '&maxResults=' + options.max,
+    url: 'https://www.googleapis.com/youtube/v3/search',
     type: 'GET',
-    // key: options.key,
-    // q: options.query,
-    // maxResults: options.max,
-    // dataType: 'jsonp',
-
-    // videoEmbeddable: true,
+    data: {
+      type: 'video',
+      part: 'snippet',
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+    },
     success: function(data) {
       console.log('IT WORKED');
+      // console.log(data);
       console.log(data.items);
-      callback(data.items);
+      return callback(data.items);
     },
     error: function(data) {
       console.log(':(');
     }
   });
-
-
-
-
 };
-
-
-
-
-
-
 
 window.searchYouTube = searchYouTube;
