ec42fa6d809d9711bb7fd47d0e391520bfe0b1dd
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-16 22:51:32 -0800

    Added additional click functionality for thumbnails / description

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,11 +1,17 @@
 var VideoListEntry = (props) => (
   <div className="video-list-entry">
     <div className="media-left media-middle">
-      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
-    </div>
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" 
+      onClick={() => props.click.call(null, props.video)}
+      />
+      </div>
     <div className="media-body">
-    <div className="video-list-entry-title" onClick={() => props.click.call(null, props.video)}>{props.video.snippet.title}</div>
-      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
+    <div className="video-list-entry-title" 
+      onClick={() => props.click.call(null, props.video)}>{props.video.snippet.title}</div>
+    <div className="video-list-entry-detail"
+      onClick={() => props.click.call(null, props.video)}>
+
+      >{props.video.snippet.description}</div>
     </div>
   </div>
 );
